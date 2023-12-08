c6ac53299f5cd00ede4711c63f7524da630f78a3
Yogesh Bhatt <me@yogicodes.com>
2023-12-08 23:44:06 +0530

    chore: add /api/v1

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
-app.get("/", (req, res) => res.json({msg: "hello, 🌍"}))
+app.use(express.json())
 
+app.get("/", (req, res) => res.json({msg: "hello, 🌍"}))
+app.use("/api/v1", routes)
 
 export default app;
