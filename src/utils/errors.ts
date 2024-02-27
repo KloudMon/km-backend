68044215fce59d3dad46ac775d45a7546d1a5c30
Yogesh Bhatt <me@yogicodes.com>
2024-02-27 22:56:19 +0530

    add: standard error handlers

--- src/utils/errors.ts
+++ src/utils/errors.ts
@@ -6,3 +6,21 @@ export class CustomError extends Error {
         this.status = status;
     }
 }
+
+export class UnauthorizedError extends CustomError {
+    constructor(message: string = 'Unauthorized') {
+        super(message, 401);
+    }
+}
+
+export class NotFoundError extends CustomError {
+    constructor(message: string = 'Not Found') {
+        super(message, 404);
+    }
+}
+
+export class ConflictError extends CustomError {
+    constructor(message: string = 'Conflict') {
+        super(message, 409);
+    }
+}
