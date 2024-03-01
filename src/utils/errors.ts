6bfe1d18a7e55157a2551fefc5c882d05de32b96
Yogesh Bhatt <me@yogicodes.com>
2024-03-01 16:30:47 +0530

    fix: Error StackTrace not being captured

--- src/utils/errors.ts
+++ src/utils/errors.ts
@@ -4,6 +4,8 @@ export class CustomError extends Error {
     constructor(message: string, status: number) {
         super(message);
         this.status = status;
+
+        Error.captureStackTrace(this, this.constructor);
     }
 }
 
