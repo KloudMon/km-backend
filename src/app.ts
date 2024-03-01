748a90cc6f7f53f42e987b11dc64385f84ceb120
Yogesh Bhatt <me@yogicodes.com>
2024-03-01 10:02:14 +0530

    add: cookieParser

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import routes from './routes';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 
 app.use(express.json())
+app.use(cookieParser())
 
 app.get("/", (req, res) => res.json({msg: "hello, 🌍"}))
 app.use("/api/v1", routes)
